5d7fec491c5d375ac3ea8ccd506f477571275095
Andrei Repin <61162669+RA404@users.noreply.github.com>
2022-11-29 23:36:08 +0400

    clear comments in PopupEditInfo

--- js/PopupEditInfo.js
+++ js/PopupEditInfo.js
@@ -8,12 +8,6 @@ class PopupEditInfo {
     this.formValidator = formValidator;
     this.userInfo = userInfo;
 
-    /** Надо исправить: вы обращаетесь в классе к переменной userInfoContainer объявленной глобально,
-    так делать нельзя. Вы можете передать эту переменную в качестве параметров, а потом уже обращаться к ней
-    Стремитесь к тому чтобы класс у вас был самодостаточным, и не зависел от глобальных переменных или классов
-    объявленных глобально, а только от тех данных которые были переданны через параметры
-    */
-    //RA404:Исправил
     this.popupEditButton = this.userInfoContainer.querySelector('.user-info__edit-btn');
     this.popupEditButton.addEventListener('click', this.openClosePopup.bind(this));
     this.form = document.forms.editForm;
@@ -26,13 +20,6 @@ class PopupEditInfo {
 
   openClosePopup() {
     this.formEditInfo.classList.toggle('popup_is-opened');
-
-    /** Надо исправить: вы обращаетесь в классе к переменной userInfo объявленной глобально,
-    так делать нельзя. Вы можете передать эту переменную в качестве параметров, а потом уже обращаться к ней
-    Стремитесь к тому чтобы класс у вас был самодостаточным, и не зависел от глобальных переменных или классов
-    объявленных глобально, а только от тех данных которые были переданны через параметры
-    */
-   //RA404: исправвил
     this.form.elements.nameResearcher.value = this.userInfo.nameResearcher;
     this.form.elements.aboutResearcher.value = this.userInfo.aboutResearcher;
 
@@ -40,26 +27,13 @@ class PopupEditInfo {
 
   saveUserInfo() {
     event.preventDefault();
-    //const userInfo = new UserInfo();
-    /** Надо исправить: вы обращаетесь в классе к переменной userInfo объявленной глобально,
-    так делать нельзя. Вы можете передать эту переменную в качестве параметров, а потом уже обращаться к ней
-    Стремитесь к тому чтобы класс у вас был самодостаточным, и не зависел от глобальных переменных или классов
-    объявленных глобально, а только от тех данных которые были переданны через параметры
-    */
-   //RA404: исправил
     this.userInfo.setUserInfo(this.form.elements.nameResearcher.value, this.form.elements.aboutResearcher.value);
     this.userInfo.updateUserInfo();
 
     this.openClosePopup();
   }
 
-  volidation(elem) {
-    /** Надо исправить: вы обращаетесь в классе к переменной formValidator объявленной глобально,
-    так делать нельзя. Вы можете передать эту переменную в качестве параметров, а потом уже обращаться к ней
-    Стремитесь к тому чтобы класс у вас был самодостаточным, и не зависел от глобальных переменных или классов
-    объявленных глобально, а только от тех данных которые были переданны через параметры
-    */
-    //RA404: исправил  
+  volidation(elem) { 
     this.formValidator.setEventListeners(elem);
   }
 }
